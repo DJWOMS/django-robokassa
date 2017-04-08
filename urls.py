c69fbae4601bb6d3fd2230d2cc5f3b43857fa329
DJWOMS <l2maximum@mail.ru>
2017-04-08 12:39:02 +0300

    Версия 0.3

--- urls.py
+++ urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from django-robokassa import views
 
 urlpatterns = [
-	url(r'^buy/$', sys_money.buy, name='buy'),
-	url(r'^popoln/$', sys_money.popoln, name='popoln'),
-	url(r'^success/$', sys_money.success, name='success'),
-	url(r'^fail/$', sys_money.fail, name='fail'),
+	url(r'^buy/$', views.buy, name='buy'),
+	url(r'^popoln/$', views.popoln, name='popoln'),
+	url(r'^success/$', views.success, name='success'),
+	url(r'^fail/$', views.fail, name='fail'),
 ]
